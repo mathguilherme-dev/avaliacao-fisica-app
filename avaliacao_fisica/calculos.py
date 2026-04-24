85dddb7e24366de3de14c496340b7acaff39fbf6
Matheus Guilherme <deliroo00@gmail.com>
2026-04-24 14:09:17 -0300

    Primeiro commit: estrutura inicial e calculos.py

---
+++ avaliacao_fisica/calculos.py
@@ -0,0 +1,83 @@
+def calcular_imc(peso, altura):
+    imc = peso / (altura * altura)
+    return imc
+
+def calcular_rcq(cintura, quadril):
+    rcq = cintura / quadril
+    return rcq
+
+def calcular_densidade(soma, idade, sexo):
+    if sexo =='M':
+        densidade = 1.112 - (0.00043499 * soma) + (0.00000055 * (soma**2)) - (0.00028826 * idade)
+    else:        densidade = 1.097 - (0.00046971 * soma) + (0.00000056 * (soma**2)) - (0.00012828 * idade)
+    return densidade
+
+def calcular_gordura(densidade):
+    gordura = ((4.95 / densidade) - 4.5) * 100
+    return gordura
+
+def classificar_imc(imc):
+    if imc < 18.5:
+        return "Abaixo do peso"
+    elif  imc < 25.0:
+        return "Peso normal"
+    elif  imc < 30.0:
+        return "Sobrepeso"
+    elif  imc < 35.0:
+        return "Obesidade I"
+    elif  imc < 40.0:
+        return "Obesidade II"
+    else:
+        return "Obesidade III"
+    
+def classificar_rcq(rcq, sexo):
+    if sexo == 'M':
+        if rcq < 0.90:
+            return 'Baixo' 
+        elif rcq < 0.95:
+            return 'Moderado'
+        elif rcq < 1.00:
+            return 'Alto'
+        else:
+            return 'Muito Alto'
+    else:
+        if  rcq < 0.80:
+            return 'Baixo'
+        elif rcq < 0.85:
+            return 'Moderado'
+        elif rcq < 0.90:
+            return 'Alto'
+        else:
+            return 'Muito Alto'
+            
+def classificar_gordura(gordura, sexo, idade):
+    if sexo =='M':
+        if idade <= 29:
+            if gordura < 11:
+                return 'Excelente'
+            elif gordura < 13:
+                return 'Muito Bom'
+            elif gordura < 16:
+                return 'Bom'
+            elif gordura < 20:
+                return 'Razoavel'
+            elif gordura < 24:
+                return 'Ruim'
+            else:                
+                return 'Muito Ruim'
+        elif idade <= 39:
+            if gordura < 13:
+                return 'Excelente'
+            elif gordura <15:
+                return 'Muito Bom'
+            elif gordura <18:
+                return 'Bom'
+            elif gordura <22:
+                return 'Razoavel'
+            elif gordura <26:
+                return 'Ruim'
+            else:
+                return 'Muito Ruim'
+            
+                
+                
