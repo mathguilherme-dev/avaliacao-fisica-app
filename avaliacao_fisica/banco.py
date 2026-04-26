400f12ddc3896522af0c95832f0dc80f5bc6f445
Matheus Guilherme <deliroo00@gmail.com>
2026-04-26 00:22:38 -0300

    feat: CRUD completo no banco.py

--- avaliacao_fisica/banco.py
+++ avaliacao_fisica/banco.py
@@ -21,3 +21,42 @@ def criar_tabelas():
     con.commit()
     con.close()
 
+def salvar_aluno(nome, idade, sexo, peso, altura):
+    con = conectar()
+    con.execute('''
+        INSERT INTO alunos (nome, idade, sexo, peso, altura)
+        VALUES (?, ?, ?, ?, ?)
+    ''', (nome, idade, sexo, peso, altura))
+    con.commit()
+    con.close()
+
+def listar_alunos():
+    con = conectar()
+    alunos = con.execute('SELECT * FROM alunos').fetchall()
+    con.close()
+    return alunos
+
+def buscar_aluno(aluno_id):
+    con = conectar()
+    aluno = con.execute('SELECT * FROM alunos WHERE id = ?',(aluno_id,)
+    ).fetchone()
+    con.close()
+    return aluno
+
+def excluir_aluno(aluno_id):
+    con = conectar()
+    con.execute(
+        'DELETE FROM alunos WHERE id = ?', (aluno_id,)
+    )
+    con.commit()
+    con.close()
+
+def atualizar_aluno(aluno_id, nome, idade, sexo, peso, altura):
+    con = conectar()
+    con.execute('''
+        UPDATE alunos
+        SET nome = ?, idade = ?, sexo = ?, peso = ?, altura = ?
+        WHERE id = ?
+    ''', (nome, idade, sexo, peso, altura, aluno_id))
+    con.commit()
+    con.close()
